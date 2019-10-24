4feec5493a7edb9ccff41ac76aac9b603e596dcb
Thor <thor@10.14.6>
2019-10-24 09:27:38 +0200

    fixed maven

---
+++ target/classes/WebStuff/cgibin/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#java -cp ./ -Dcgi.content_type=$CONTENT_TYPE -Dcgi.content_length=$CONTENT_LENGTH -Dcgi.query_string=$QUERY_STRING -Dcgi.server_name=$SERVER_NAME -Dcgi.script_name=$SCRIPT_NAME -Dcgi.path_info=$PATH_INFO CgiTest
+QUERY_STRING=`echo $QUERY_STRING | cut -d\& -f1`
+java -cp ./ -Dquery_string=$QUERY_STRING CgiTest
