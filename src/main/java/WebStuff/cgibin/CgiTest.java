7e03bc526805d804d159a1368d049b61798ea802
Thor <thor@10.14.6>
2019-10-24 14:09:17 +0200

    fixed pom

--- src/main/java/WebStuff/cgibin/CgiTest.java
+++ src/main/java/WebStuff/cgibin/CgiTest.java
@@ -4,9 +4,15 @@ public class CgiTest {
 		String opts = System.getProperty("query_string");
 		String type = "Content-Type: text/html\n\n";
 		String output = "<html>" +
+			"<BODY>" +
+			"<b>Simple Web Form</b><p>" +
 			"<p>Kurt Verner Bor her </p>" +
 			"<p>opt: " +  opts + "</p>" +
-			"<p>Hi there </p>" +
+			"<FORM ACTION=\"/cgi-bin/run.sh\">" +
+			"<INPUT TYPE=\"TEXT\" NAME=\"Field\" SIZE=\"25\"><BR>"+
+			"<INPUT TYPE=\"SUBMIT\" NAME=\"Submit\" VALUE=\"Submit\">"+
+			"</FORM>"+
+			"</BODY>"+
 			"</html>";
 		System.out.println(type);
 		System.out.println(output);
