23acc63cd2383de94abd467d0a49fc61f9c2c2e8
Thor <thor@10.14.6>
2019-10-24 10:28:39 +0200

    fixed antrun

--- target/classes/WebStuff/cgibin/run.sh
+++ target/classes/WebStuff/cgibin/run.sh
@@ -2,4 +2,4 @@
 
 #java -cp ./ -Dcgi.content_type=$CONTENT_TYPE -Dcgi.content_length=$CONTENT_LENGTH -Dcgi.query_string=$QUERY_STRING -Dcgi.server_name=$SERVER_NAME -Dcgi.script_name=$SCRIPT_NAME -Dcgi.path_info=$PATH_INFO CgiTest
 QUERY_STRING=`echo $QUERY_STRING | cut -d\& -f1`
-java -cp ./ -Dquery_string=$QUERY_STRING CgiTest
+java -jar -Dquery_string=$QUERY_STRING CarRentalWeb-0.0.1.jar
